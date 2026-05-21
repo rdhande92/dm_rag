ccb05e811f431cd10b7511d806c3e881ba01aa44
Rushikesh <rdhande92@gmail.com>
2026-05-21 22:09:36 +0530

    adding test case

---
+++ test_app.py
@@ -0,0 +1,16 @@
+import unittest
+from app import app
+
+class BasicTests(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_home_route(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'<!DOCTYPE html>', response.data)
+
+if __name__ == "__main__":
+    unittest.main()
